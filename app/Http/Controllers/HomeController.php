b39b181706d9091bc40d8c2f0b132e48ca897587
m.hayakawa <m.hayakawa.ba@gmail.com>
2023-03-11 00:06:51 +0900

    キャッシュを入れてみた

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
 use App\Services\News\NewsReadService;
 use App\Services\Program\ProgramReadService;
 use App\Services\RecommendQuery\RecommendQueryReadService;
@@ -39,40 +40,55 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //クロージャに渡す用の変数を用意
+        $that = $this;
+
         //おすすめリンクを取得
         $recommend_queries = $this->recommendQueryReadService->getRecommendQueries();
 
         //最新ニュースを取得
-        $newses = $this->newsReadService->getNewsAtHome(
-            $this->max_news_count
-        );
+        $newses = Cache::remember('home_newses', 60*60, function() use ($that) {
+            return $that->newsReadService->getNewsAtHome(
+                $that->max_news_count
+            );
+        });
 
         //今週のランキングを取得
-        $rankings = $this->programReadService->getTotalRanking(
-            $this->max_ranking_count,
-            $this->period_ranking,
-        );
+        $rankings = Cache::remember('home_rankings', 60*60, function() use ($that) {
+            return $that->programReadService->getTotalRanking(
+                $that->max_ranking_count,
+                $that->period_ranking,
+            );
+        });
 
         //新着動画を取得
-        $youtube_programs = $this->programReadService->getLatestProgramsEverySite(
-            $this->max_program_count,
-            config('const.site.youtube'),
-        );
-        $nicovideo_programs = $this->programReadService->getLatestProgramsEverySite(
-            $this->max_program_count,
-            config('const.site.nicovideo'),
-        );
+        $youtube_programs = Cache::remember('home_youtube_programs', 60*60, function() use ($that) {
+            return $that->programReadService->getLatestProgramsEverySite(
+                $that->max_program_count,
+                config('const.site.youtube'),
+            );
+        });
+        $nicovideo_programs = Cache::remember('home_nicovideo_programs', 60*60, function() use ($that) {
+            return $that->programReadService->getLatestProgramsEverySite(
+                $that->max_program_count,
+                config('const.site.nicovideo'),
+            );
+        });
 
         //おすすめレビューを取得
-        $reviews = $this->reviewReadService->getReviewsAtHome(
-            $this->max_review_count
-        );
+        $reviews = Cache::remember('home_reviews', 60*60, function() use ($that) {
+            return $that->reviewReadService->getReviewsAtHome(
+                $that->max_review_count
+            );
+        });
 
         //人気の検索ワードを取得
-        $search_words = $this->searchWordReadService->getSearchWords(
-            $this->max_search_word,
-            $this->period_search_word,
-        );
+        $search_words = Cache::remember('home_search_words', 60*60, function() use ($that) {
+            return $that->searchWordReadService->getSearchWords(
+                $that->max_search_word,
+                $that->period_search_word,
+            );
+        });
 
         //viewへ遷移
         return Inertia::render('Home/Index', compact(
