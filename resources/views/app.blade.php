ebc5340bbd264a6bbd73465dc2d9c6fe71b2c3f2
m.hayakawa <m.hayakawa.ba@gmail.com>
2023-03-10 22:19:10 +0900

    タグマネを入れた

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
+        {{-- googleタグマネージャー --}}
+        @if (config('app.env') == 'production')
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P646PW5');</script>
+            <!-- End Google Tag Manager -->
+        @endif
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
         <link href="{{ mix('/css/user.css') }}?v=15" rel="stylesheet" />
@@ -8,6 +18,11 @@
         @inertiaHead
     </head>
     <body>
+        {{-- googleタグマネージャー --}}
+        @if (config('app.env') == 'production')
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P646PW5"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        @endif
         @inertia
     </body>
 </html>
